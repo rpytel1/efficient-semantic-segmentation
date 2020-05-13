7db5af6c80543b94d21872d6c0523b4bfc30e3f6
rpytel1 <rafal.pytel1@wp.pl>
2020-05-13 17:14:34 +0000

    Added evaluation on val of Cityscapes

---
+++ prepare_data.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+from glob import glob
+
+def sanitize_data(directory):
+    images = glob(directory + '/leftImg8bit/*/*.png')
+
+    for img in images:
+        new = img.replace("_leftImg8bit", "")
+        os.rename(img,new)
+
+    gt = glob(directory + '/gtFine/*/*.png')
+
+    for img in gt:
+        new = img.replace("_gtFine_labelIds", "")
+        os.rename(img,new)
+
+## Flatten cityscapes
+for gfolder in ['gtFine','leftImg8bit']:
+    for folder in ['train', 'val','test']:
+        dir = os.path.join("data/cityspaces/", gfolder, folder)
+        directories=[d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir,d))]
+
+        for d in directories:
+            files = os.listdir(os.path.join(dir,d))
+            for f in files:
+                if f.endswith(".png") or f.endswith(".json"):
+                    what = os.path.join(dir,d,f)
+                    to = os.path.join(dir)
+                    shutil.move(what, to)
+
+sanitize_data("data/minicity")
+sanitize_data("data/cityspaces")
